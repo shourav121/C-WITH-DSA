19d87fddd292ce438d841c15539a42d28202cc45
shourav kumar <shouravk81@gmail.com>
2024-09-17 22:30:06 +0530

    work

---
+++ CONDITIONAL/ifelse1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=44;
+
+    if(n>=0)
+    {
+        cout<<"this is +ve number";
+    }
+    else{
+        cout<<"this is -ve number";
+    }
+    return 0;
+}
