67e7eae72ff7cdccff534f32d3d62d47577bf1a0
shourav kumar <shouravk81@gmail.com>
2024-09-17 22:55:50 +0530

    work

---
+++ CONDITIONAL/even.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a number: ";
+    cin>>n;
+
+    if(n%2==0)
+    {
+        cout<<"this is even number";
+
+    }
+    else{
+        cout<<"this is odd number";
+    }
+    return 0;
+}
